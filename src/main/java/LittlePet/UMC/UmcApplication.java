f6cd310027eb1217a47f86489bf6e723583e3a71
jjun522 <tjwowns522@>
2025-01-06 00:48:51 +0900

    feat: 템플릿 작성 및 초반 세팅

---
+++ src/main/java/LittlePet/UMC/UmcApplication.java
@@ -0,0 +1,13 @@
+package LittlePet.UMC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcApplication.class, args);
+	}
+
+}
