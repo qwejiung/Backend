f14e0ca81cd7c002c190e285f5b845601af0f35c
HwangHyoeun <hhe5361@naver.com>
2025-01-10 13:46:32 +0900

    move test properties file

--- src/test/java/LittlePet/UMC/repository/UserRepositoryTest.java
+++ src/test/java/LittlePet/UMC/repository/UserRepositoryTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-import static org.junit.jupiter.api.Assertions.*;
 @TestPropertySource(locations = "classpath:application-test.properties")
 @SpringBootTest(classes = UmcApplication.class)
 public class UserRepositoryTest {
