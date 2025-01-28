11c38dfeba808c36f4a3752fb795622d2113e881
jjun522 <tjwowns522@>
2025-01-28 13:27:55 +0900

    fix: 오류해결

--- src/main/java/LittlePet/UMC/SmallPet/repository/PetCategoryRepository.java
+++ src/main/java/LittlePet/UMC/SmallPet/repository/PetCategoryRepository.java
@@ -2,6 +2,8 @@ package LittlePet.UMC.SmallPet.repository;
 
 import LittlePet.UMC.domain.petEntity.categories.PetCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PetCategoryRepository extends JpaRepository<PetCategory, Long> {
 }
