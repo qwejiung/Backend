f87e8a28c52ad4991585895c9a2c50e20d807c51
jjun522 <tjwowns522@>
2025-02-04 18:08:59 +0900

    feat: url 수정

--- src/main/java/LittlePet/UMC/User/controller/UserProfileController.java
+++ src/main/java/LittlePet/UMC/User/controller/UserProfileController.java
@@ -12,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserProfileController {
 
