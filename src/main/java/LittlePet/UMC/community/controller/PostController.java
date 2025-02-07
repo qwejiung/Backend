de5ea5f4a13912e6495a3f55e68c225cc1fadaa1
qwejiung <qq8758@naver.com>
2025-02-08 01:40:36 +0900

    fix: postController 수정

--- src/main/java/LittlePet/UMC/community/controller/PostController.java
+++ src/main/java/LittlePet/UMC/community/controller/PostController.java
@@ -31,11 +31,15 @@ public class PostController {
     private final S3Service s3Service;
 
     @Operation(summary = "커뮤니티 게시물 생성", description = "커뮤니티에 새로운 게시물을 생성할 수 있는 카테고리입니다")
-    @PostMapping(value = "/api/post/{user-id}", consumes = {"multipart/form-data"})
+    @PostMapping(value = "/post/{user-id}", consumes = {"multipart/form-data"})
     public ApiResponse<CreatePostResponseDTO> createPost(
             @RequestPart @Valid PostForm postForm,
             @RequestPart(value = "images", required = false) List<MultipartFile> images,
             @PathVariable("user-id") Long userId) throws IOException {
+
+//        if (images.size() > 5) {
+//             // 예외 발생
+//        }
 
         List<String> imageUrls = new ArrayList<>();
         if (images != null || !images.isEmpty()) {
@@ -52,7 +56,7 @@ public class PostController {
     }
 
     @Operation(summary = "커뮤니티 게시물 수정", description = "커뮤니티에 기존 게시물을 수정할 수 있는 카테고리입니다")
-    @PatchMapping(value = "/api/post/{post-id}", consumes = {"multipart/form-data"})
+    @PatchMapping(value = "/post/{post-id}", consumes = {"multipart/form-data"})
     public ApiResponse<CreatePostResponseDTO> updatePost(
             @PathVariable("post-id") Long postId,
             @RequestPart @Valid PostForm postForm,
@@ -74,14 +78,14 @@ public class PostController {
     }
 
     @Operation(summary = "커뮤니티 게시물 삭제", description = "커뮤니티를 삭제할 수 있는 카테고리입니다")
-    @DeleteMapping("/api/post/{post-id}")
+    @DeleteMapping("/post/{post-id}")
     public ApiResponse<String> deletePost(@PathVariable("post-id") Long postId) {
         postService.deletePost(postId);
         return ApiResponse.onSuccess("게시물이 성공적으로 삭제되었습니다");
     }
 
     @Operation(summary = "커뮤니티 특정 글 조회", description = "특정 게시물을 조회하는 카테고리입니다")
-    @GetMapping("/api/post/{post-id}")
+    @GetMapping("/post/{post-id}")
     public ApiResponse<GetPostResponseDTO> GetPost(@PathVariable("post-id") Long postId) {
         Post post = postService.FindOnePost(postId);
 
